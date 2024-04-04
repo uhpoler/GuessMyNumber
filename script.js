42931144dae1c1206dfccedb912161fac891a26d
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-04-04 12:36:16 +0300

    manipulating CSS styles

--- script.js
+++ script.js
@@ -21,10 +21,18 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
+  //When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number';
+
+    //When the player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 0) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -33,6 +41,8 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent = '💥 You lost the game!';
     }
+
+    //When guess is too low
   } else if (guess < secretNumber) {
     if (score > 0) {
       document.querySelector('.message').textContent = '📉 Too low!';
